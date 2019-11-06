72647cdb36d720aa18628b828db4d028d157cde6
Robert Lieck <science@robert-lieck.com>
2019-11-06 17:08:12 +0100

    add chordify utility function

--- CorpusInterface/util.py
+++ CorpusInterface/util.py
@@ -1,4 +1,4 @@
-from CorpusInterface.datatypes import MIDIPitch
+from CorpusInterface.datatypes import MIDIPitch, Event
 
 
 def split_midi_by_parts(events):
@@ -15,3 +15,33 @@ def split_midi_by_parts(events):
             part_list.append([])
         part_list[part_indices[part]].append(e)
     return part_list
+
+
+def chordify(piece):
+    # create dictionary with time on- and offsets and events starting at a certain onset
+    event_dict = {}
+    for e in piece:
+        # add onset and offset time slot
+        if not e.time in event_dict:
+            event_dict[e.time] = []
+        if not e.time + e.duration in event_dict:
+            event_dict[e.time + e.duration] = []
+        # add event to onset time slot
+        event_dict[e.time].append(e)
+    # turn dict into ordered list of time-events pairs
+    event_list = list(sorted(event_dict.items(), key=lambda item: item[0]))
+    # take care of events that extend over multiple time slots
+    active_events = []
+    for time, events in event_list:
+        # from the active events (that started earlier) only keep those that reach into current time slot
+        active_events = [event for event in active_events if event.time + event.duration > time]
+        # add these events to the current time slot
+        events += active_events
+        # remember events that start with this slot to possibly add them in later slots
+        active_events += events
+    # the last element should not contain any events as it was only created because the last event(s) ended at that time
+    if event_list[-1][1]:
+        raise ValueError("The last time slot should be empty. This is a bug (maybe due to floating point arithmetics?)")
+    # turn dict into an ordered list of events with correct durations and combined event data
+    return [Event(time=time, duration=next_time - time, data=frozenset([e.data for e in events]))
+            for (time, events), (next_time, next_events) in zip(event_list, event_list[1:])]
